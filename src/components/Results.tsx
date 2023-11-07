b66201c72b28253d384f5c8e64939b8752c14b3c
unknown <murilodepontesribeiro02@gmail.com>
2023-11-07 20:18:29 -0300

    Quiz

---
+++ src/components/Results.tsx
@@ -0,0 +1,23 @@
+import { Questions } from "@/types/Questions";
+
+type Props = {
+    questions: Questions[];
+    answers: number[];
+
+}
+
+export const Results = ({questions, answers}: Props) => {
+    return(
+        <div>
+            {questions.map((item, key) => (
+                <div key={key} className="mb-3">
+                    <div className="font-bold">{key + 1}. {item.question}</div>
+                    <div>
+                        <span>({item.answer === answers[key] ? 'Acertouu' : 'Errouu!!'}) - </span>
+                        {item.options[item.answer]}
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
---
+++ src/data/questions.ts
@@ -0,0 +1,24 @@
+import { Questions } from "@/types/Questions";
+
+export const questions: Questions[] = [
+    {
+        question: 'Qual ingrediente não pode faltar?',
+        options: [
+            'Farinha',
+            'Corante',
+            'Agua',
+            'Açucar'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual é o melhor método de cozinhar?',
+        options: [
+            'Microondas',
+            'Fogão',
+            'Fogueira',
+            'Lança-chamas'
+        ],
+        answer: 3
+    }
+];
